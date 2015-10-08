5a09c67e743d0f1467a84ede3169cc1f8426a7d7
franksin101 <franksin101@gmail.com>
2015-10-09 07:12:47 +0800

    Main
    Main Program

---
+++ Main.py
@@ -0,0 +1,9 @@
+import xml.etree.ElementTree as XMLParser
+import urllib.request as HttpRequest
+
+HttpRequestHeaders = {  "Content-type": "application/x-www-form-urlencoded", 
+						"Accept": "text/html",
+						"User-Agent": ""}
+
+if __name__ == "__main__" :
+	print("I am main function")
